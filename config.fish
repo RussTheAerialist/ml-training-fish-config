1bfa88bd018d89dc7f58d6cd64a3a5b06050d5ee
Russell Hay <russell.hay@gmail.com>
2020-08-31 21:22:41 -0700

    Adds check to see if rust is installed

--- config.fish
+++ config.fish
@@ -1,11 +1,13 @@
 source ~/.cargo/env
 
-alias cat='~/.cargo/bin/bat'
-alias ls='~/.cargo/bin/exa --icons'
-alias ll='~/.cargo/bin/exa --icons -l'
-alias find='~/.cargo/bin/fd'
-alias ps='~/.cargo/bin/procs'
-alias sed='~/.cargo/bin/sd'
-alias du='~/.cargo/bin/dust'
-alias grep='~/.cargo/bin/rg'
-alias top='~/.cargo/bin/ytop'
+if test -e ~/.cargo/bin
+    alias cat='~/.cargo/bin/bat'
+    alias ls='~/.cargo/bin/exa --icons'
+    alias ll='~/.cargo/bin/exa --icons -l'
+    alias find='~/.cargo/bin/fd'
+    alias ps='~/.cargo/bin/procs'
+    alias sed='~/.cargo/bin/sd'
+    alias du='~/.cargo/bin/dust'
+    alias grep='~/.cargo/bin/rg'
+    alias top='~/.cargo/bin/ytop'
+end
